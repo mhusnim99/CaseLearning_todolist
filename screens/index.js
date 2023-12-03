47a751f67880d8ccad2f64ef0a91b4763f5deee9
Muhammad Husni Mubarrok <mubarrokhusni12@gmail.com>
2023-12-03 19:56:59 +0700

    first commit

---
+++ screens/index.js
@@ -0,0 +1,4 @@
+import AboutScreen from "./about";
+import TaskScreen from "./task_all";
+import TaskCompletedScreen from "./task_completed";
+export { AboutScreen, TaskCompletedScreen, TaskScreen };
